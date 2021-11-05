59da300861c2fbf8b10139edff639d74f74cbb97
Rahman Zulkarnaen <92029691+rhmnz15@users.noreply.github.com>
2021-11-05 16:52:37 +0700

    Update no_1.js

--- Tugas 5/no_1.js
+++ Tugas 5/no_1.js
@@ -5,15 +5,32 @@ Program Array
 */
 
 
-//menyusun array berdasarkan abjad
-let Fruits = [ 'Apel', 'Pisang', 'Jambu', 'Anggur', 'Pepaya', 'Duku', 'Durian', 'Jeruk', 'Mangga', 'Manggis'];
-let buah =[]
+let fruits = new Array ("Apel", "Pisang", "Jambu", "Anggur", "Pepaya", "Duku", "Durian", "Jeruk", "Mangga", "Manggis");
+let boxA = []
+let boxP = []
+let boxJ = []
+let boxD = []
+let boxM = []
+for(let i = 0; i< fruits.length; i++){
+    let hurufAwal = fruits[i].substring(0,1);
 
-for(let i=0;< Fruits.length;i++){
-    let hurufAwal = Fruits[i].substring(0,1);
-    if(hurufAwal=="A"){
-        buah.push(Fruits[i]);
+    if (hurufAwal == "A") {
+        boxA.push(fruits[i]);
+    }else if (hurufAwal == "P" ) {
+        boxP.push(fruits[i])
+    }else if (hurufAwal == "J") {
+        boxJ.push(fruits[i])
+    }else if (hurufAwal == "D") {
+        boxD.push(fruits[i])
+    }else if (hurufAwal == "M") {
+        boxM.push(fruits[i])
+    }else{
+        console.log("Tidak Ada Data");
     }
 }
 
-console.log(buah);
+console.log(boxA);
+console.log(boxP);
+console.log(boxJ);
+console.log(boxD);
+console.log(boxM);
